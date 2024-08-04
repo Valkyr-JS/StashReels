9412548fe7da1f6a0c281a030d5e8783964221a7
ValkyrJS <valkyr.photos@gmail.com>
2024-08-05 00:50:03 +0100

    Fetch data from Stash

---
+++ src/helpers/index.ts
@@ -0,0 +1,18 @@
+/** Fetch data from Stash via GQL. */
+export const fetchData = async (query: string) => {
+  try {
+    const res = await fetch(
+      process.env.NODE_ENV === "production"
+        ? "/graphql"
+        : process.env.STASH_ADDRESS + "/graphql",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query }),
+      }
+    );
+    return await res.json();
+  } catch (err) {
+    return console.log(err);
+  }
+};
