1b53b5299590d3ab4726f05249e154a9fcfe3248
Valkyr-JS <154020147+Valkyr-JS@users.noreply.github.com>
2024-08-05 00:53:56 +0100

    Fetch data from Stash (#2)

--- src/components/VideoItem/index.tsx
+++ src/components/VideoItem/index.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as styles from "./VideoItem.module.scss";
+import { fetchData } from "../../helpers";
 
 interface VideoItemProps {}
 
 const VideoItem: React.FC<VideoItemProps> = (props) => {
-  console.log("Loading videoitem: ", props);
-  return <div className={styles.base}>Video Item</div>;
+  const [sceneData, setSceneData] = useState<{
+    data: { findScenes: FindScenesResultType };
+  } | null>(null);
+
+  // Fetch scene data
+  useEffect(() => {
+    fetchData(`{
+      findScenes(
+        filter: { per_page: 5, sort: "random" }
+        scene_filter: { orientation: { value: PORTRAIT } }
+      ) {
+        scenes {
+          id
+          files { format }
+          paths { stream }
+          title
+          }
+        }
+      }`).then((res) => setSceneData(res));
+  }, []);
+
+  return (
+    <div className={styles.base}>
+      {sceneData?.data.findScenes.scenes[0].title || "Loading"}
+    </div>
+  );
 };
 
 export default VideoItem;
