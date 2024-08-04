8a6fcba2b77913e43be48daf4c4b4889c8137f75
ValkyrJS <valkyr.photos@gmail.com>
2024-08-04 23:32:57 +0100

    SCSS modules

--- src/components/VideoItem/index.tsx
+++ src/components/VideoItem/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import * as styles from "./VideoItem.module.scss";
 
 interface VideoItemProps {}
 
 const VideoItem: React.FC<VideoItemProps> = (props) => {
   console.log("Loading videoitem: ", props);
-  return <div>Video Item</div>;
+  return <div className={styles.base}>Video Item</div>;
 };
 
 export default VideoItem;
