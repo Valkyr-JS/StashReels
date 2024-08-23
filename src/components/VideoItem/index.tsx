0693506e5a4b790776d5fbccf6ff85492943ffa1
ValkyrJS <valkyr.photos@gmail.com>
2024-08-23 17:36:55 +0100

    UI button component

--- src/components/VideoItem/index.tsx
+++ src/components/VideoItem/index.tsx
@@ -27,7 +27,11 @@ import * as styles from "./VideoItem.module.scss";
 import "./VideoItem.scss";
 import { useIsInViewport } from "../../hooks";
 import { secondsToTimestamp, sortPerformers } from "../../helpers";
-import { faPause, faPlay } from "@fortawesome/pro-solid-svg-icons";
+import {
+  faPause,
+  faPlay,
+  IconDefinition,
+} from "@fortawesome/pro-solid-svg-icons";
 import { TRANSITION_DURATION } from "../../constants";
 
 export interface VideoItemProps extends IitemData {
@@ -194,13 +198,15 @@ const VideoItem: React.FC<VideoItemProps> = (props) => {
     !!props.scene.date;
 
   const sceneInfoButton = sceneInfoDataAvailable ? (
-    <button
+    <UiButton
+      active={sceneInfoOpen}
+      activeIcon={faCircleInfo}
+      activeText="Close scene info"
       data-testid="VideoItem--infoButton"
+      inactiveIcon={faCircleInfoOff}
+      inactiveText="Show scene info"
       onClick={sceneInfoButtonClickHandler}
-      type="button"
-    >
-      <FontAwesomeIcon icon={sceneInfoOpen ? faCircleInfo : faCircleInfoOff} />
-    </button>
+    />
   ) : null;
 
   /* -------------------------------- Scrubber -------------------------------- */
@@ -257,15 +263,15 @@ const VideoItem: React.FC<VideoItemProps> = (props) => {
       : null;
 
   const subtitlesButton = !!captionSources ? (
-    <button
+    <UiButton
+      active={props.subtitlesOn}
+      activeIcon={faSubtitles}
+      activeText="Hide subtitles"
       data-testid="VideoItem--subtitlesButton"
+      inactiveIcon={faSubtitlesOff}
+      inactiveText="Show subtitles"
       onClick={props.toggleSubtitlesHandler}
-      type="button"
-    >
-      <FontAwesomeIcon
-        icon={props.subtitlesOn ? faSubtitles : faSubtitlesOff}
-      />
-    </button>
+    />
   ) : null;
 
   // Update the subtitles track via the ref object
@@ -350,50 +356,49 @@ const VideoItem: React.FC<VideoItemProps> = (props) => {
                 ...toggleableUiTransitionStyles[state],
               }}
             >
-              <button
+              <UiButton
+                active={props.isMuted}
+                activeIcon={faVolumeOff}
+                activeText="Unmute"
                 data-testid="VideoItem--muteButton"
+                inactiveIcon={faVolume}
+                inactiveText="Mute"
                 onClick={muteButtonClickHandler}
-                type="button"
-              >
-                <FontAwesomeIcon
-                  icon={props.isMuted ? faVolumeOff : faVolume}
-                />
-                <span className={styles["visually-hidden"]}>
-                  {props.isMuted ? "Unmute" : "Mute"}
-                </span>
-              </button>
+              />
 
               {subtitlesButton}
 
-              <button
+              <UiButton
+                active={props.isFullscreen}
+                activeIcon={faExpand}
+                activeText="Close fullscreen"
                 data-testid="VideoItem--fullscreenButton"
+                inactiveIcon={faExpandOff}
+                inactiveText="Open fullscreen"
                 onClick={fullscreenButtonClickHandler}
-                type="button"
-              >
-                <FontAwesomeIcon
-                  icon={props.isFullscreen ? faExpand : faExpandOff}
-                />
-              </button>
+              />
 
-              <button
+              <UiButton
+                active={props.loopOnEnd}
+                activeIcon={faRepeat}
+                activeText="Stop looping scene"
                 data-testid="VideoItem--loopButton"
+                inactiveIcon={faRepeatOff}
+                inactiveText="Loop scene"
                 onClick={loopButtonClickHandler}
-                type="button"
-              >
-                <FontAwesomeIcon
-                  icon={props.loopOnEnd ? faRepeat : faRepeatOff}
-                />
-              </button>
+              />
 
               {sceneInfoButton}
 
-              <button
+              <UiButton
+                active={false}
+                activeIcon={faGearOff}
+                activeText="Close settings"
                 data-testid="VideoItem--settingsButton"
+                inactiveIcon={faGearOff}
+                inactiveText="Show settings"
                 onClick={() => props.setSettingsTabHandler(true)}
-                type="button"
-              >
-                <FontAwesomeIcon icon={faGearOff} />
-              </button>
+              />
             </div>
           )}
         </Transition>
@@ -403,19 +408,21 @@ const VideoItem: React.FC<VideoItemProps> = (props) => {
           timeout={TRANSITION_DURATION}
         >
           {(state) => (
-            <button
+            <UiButton
+              active={props.uiIsVisible}
+              activeIcon={faXmark}
+              activeText="Hide UI"
               className={styles["toggleable-ui-button"]}
               data-testid="VideoItem--showUiButton"
+              inactiveIcon={faBars}
+              inactiveText="Show UI"
               onClick={toggleUiButtonClickHandler}
               ref={uiButtonRef}
-              type="button"
               style={{
                 ...toggleableUiStyles,
                 ...toggleUiButtonTransitionStyles[state],
               }}
-            >
-              <FontAwesomeIcon icon={props.uiIsVisible ? faXmark : faBars} />
-            </button>
+            />
           )}
         </Transition>
       </div>
@@ -450,6 +457,41 @@ const VideoItem: React.FC<VideoItemProps> = (props) => {
 };
 
 export default VideoItem;
+
+/* -------------------------------------------------------------------------- */
+/*                                  UI Button                                 */
+/* -------------------------------------------------------------------------- */
+
+interface UiButtonProps
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  /** Indicates if the buttons associated action is active. */
+  active: boolean;
+  activeIcon: IconDefinition;
+  activeText: string;
+  inactiveIcon: IconDefinition;
+  inactiveText: string;
+}
+
+const UiButton: React.FC<UiButtonProps> = ({
+  active,
+  activeIcon,
+  activeText,
+  inactiveIcon,
+  inactiveText,
+  ...props
+}) => {
+  return (
+    <button {...props} onClick={props.onClick} type="button">
+      <FontAwesomeIcon icon={active ? activeIcon : inactiveIcon} />
+      <span className={styles["visually-hidden"]}>
+        {active ? activeText : inactiveText}
+      </span>
+    </button>
+  );
+};
 
 /* -------------------------------------------------------------------------- */
 /*                              Scene info panel                              */
