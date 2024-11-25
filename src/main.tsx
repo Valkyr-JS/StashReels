cb99d76798ca8ac34df8b2137f986adc4cce2296
Valkyr-JS <154020147+Valkyr-JS@users.noreply.github.com>
2024-11-25 14:42:51 +0000

    fix: Nav button doesn't render after initial install (#28)

--- src/main.tsx
+++ src/main.tsx
@@ -1,69 +1,61 @@
-import React from "react";
 import { faMobile } from "@fortawesome/pro-solid-svg-icons/faMobile";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { createRoot } from "react-dom/client";
 import { fetchPluginConfig } from "./helpers";
 import { PLUGIN_NAMESPACE } from "./constants";
 
-/** https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists */
-function waitForElm(selector: string) {
-  return new Promise<HTMLElement>((resolve) => {
-    if (document.querySelector(selector)) {
-      return resolve(document.querySelector(selector) as HTMLElement);
-    }
+const { PluginApi } = window;
+const { React } = PluginApi;
 
-    const observer = new MutationObserver((mutations) => {
-      if (document.querySelector(selector)) {
-        observer.disconnect();
-        resolve(document.querySelector(selector) as HTMLElement);
+// Wait for the navbar to load, as this contains the
+PluginApi.patch.instead(
+  "MainNavBar.MenuItems",
+  function ({ children, ...props }, _, Original) {
+    const [showButton, setShowButton] = React.useState(true);
+
+    fetchPluginConfig().then((res) => {
+      // Check if plugin config exists
+      if (res?.data && res.data.configuration.plugins[PLUGIN_NAMESPACE]) {
+        const pluginConfig: PluginConfig | undefined =
+          res.data.configuration.plugins[PLUGIN_NAMESPACE];
+
+        setShowButton(!pluginConfig.hideNavButton);
       }
     });
 
-    // If you get "parameter 1 is not of type 'Node'" error, see
-    // https://stackoverflow.com/a/77855838/492336
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  });
-}
+    // If data isn't yet available or the user has hidden the button, return the
+    // original component
+    if (!showButton) return [<Original {...props} children={children} />];
 
-const link = "/plugin/" + PLUGIN_NAMESPACE + "/assets/app/";
-const reelsButton = document.createElement("div");
-
-reelsButton.id = "StashReelsButton";
-reelsButton.setAttribute("data-rb-event-key", link);
-reelsButton.className = "col-4 col-sm-3 col-md-2 col-lg-auto nav-link";
-
-const ReelsButtonInner = () => (
-  <a
-    href={link}
-    className="minimal p-4 p-xl-2 d-flex d-xl-inline-block flex-column justify-content-between align-items-center btn btn-primary"
-    target="_blank"
-  >
-    <FontAwesomeIcon
-      className="fa-icon nav-menu-icon d-block d-xl-inline mb-2 mb-xl-0"
-      icon={faMobile}
-    />
-    <span>Reels</span>
-  </a>
+    // Add the button to the navbar
+    return [
+      <Original {...props}>
+        {children}
+        <ReelsButtonInner />
+      </Original>,
+    ];
+  }
 );
 
-/* ---------------------------------- Init ---------------------------------- */
+const ReelsButtonInner = () => {
+  const link = "/plugin/" + PLUGIN_NAMESPACE + "/assets/app/";
 
-fetchPluginConfig().then((res) => {
-  // Check if plugin config exists
-  if (res.data.configuration.plugins[PLUGIN_NAMESPACE]) {
-    const pluginConfig = res.data.configuration.plugins[
-      PLUGIN_NAMESPACE
-    ] as PluginConfig;
-
-    if (pluginConfig.hideNavButton !== true) {
-      waitForElm(".top-nav .navbar-nav").then((elm) => {
-        elm.appendChild(reelsButton);
-        const root = createRoot(reelsButton!);
-        root.render(<ReelsButtonInner />);
-      });
-    }
-  }
-});
+  return (
+    <div
+      data-rb-event-key={link}
+      className="col-4 col-sm-3 col-md-2 col-lg-auto nav-link"
+      id="StashReelsButton"
+    >
+      <a
+        href={link}
+        className="minimal p-4 p-xl-2 d-flex d-xl-inline-block flex-column justify-content-between align-items-center btn btn-primary"
+        target="_blank"
+      >
+        <FontAwesomeIcon
+          className="fa-icon nav-menu-icon d-block d-xl-inline mb-2 mb-xl-0"
+          icon={faMobile}
+        />
+        <span>Reels</span>
+      </a>
+    </div>
+  );
+};
