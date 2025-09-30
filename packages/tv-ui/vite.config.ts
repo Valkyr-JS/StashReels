db5874be02b569f2f192b6d647f25568af497f3b
Second Folder <secondfolder@gmail.com>
2025-09-30 14:07:17 +1000

    Add global dev start command

--- packages/tv-ui/vite.config.ts
+++ packages/tv-ui/vite.config.ts
@@ -6,11 +6,18 @@ export default defineConfig( ({mode}) => {
   // Set the third parameter to '' to load all env regardless of the
   // `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
+
+  const port = env.DEV_PORT ? parseInt(env.DEV_PORT) : undefined;
+  const host = env.DEV_HOST?.toLowerCase() === "true" ? true : env.DEV_HOST
+  const allowedHosts = env.DEV_ALLOWED_HOSTS?.toLowerCase() === "true" ? true : env.DEV_ALLOWED_HOSTS?.split(",");
+
   return {
     root: "src",
     envDir: "..",
     server: {
-      port: env.VITE_APP_PLATFORM_PORT ? parseInt(env.VITE_APP_PLATFORM_PORT) : undefined,
+      port,
+      host,
+      allowedHosts,
       // proxy: {
       //   // Proxy all requests starting with /graphql to the target server
       //   '/graphql': {
