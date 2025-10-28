d3a0261361fec6946a7a669b31e232abed9fb6fc
Second Folder <secondfolder@gmail.com>
2025-10-28 11:10:13 +1100

    chore: remove duplicate styles

--- packages/tv-ui/src/index.tsx
+++ packages/tv-ui/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
-import "./styles/globals.scss";
 import "stash-ui/dist/theme.css";
+import "./styles/globals.scss";
 
 import { ApolloProvider } from "@apollo/client";
 import { getApolloClient } from "./hooks/getApolloClient";
