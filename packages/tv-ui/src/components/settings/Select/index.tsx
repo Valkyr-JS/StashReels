0bd1b7493f4543364841e8e1c2e9b6432a79ba50
Second Folder <secondfolder@gmail.com>
2025-11-09 20:53:57 +1100

    fix: make sure dropdown is always on top

--- packages/tv-ui/src/components/settings/Select/index.tsx
+++ packages/tv-ui/src/components/settings/Select/index.tsx
@@ -10,11 +10,17 @@ const Select: RSSelect = (props) => {
   // Disabling isSearchable on mobile resolves the issue of keyboard popping up on mobile devices and messing up the layout, particularly for forceLandscape mode
   const isSearchable = props.isSearchable !== undefined ? props.isSearchable : !hasTouchScreen;
 
-  // We use the "react-select" class name so that stash styles are applied
+  // We use the "react-select" class name so that stash styles are applied and we use menuPortalTarget to render the
+  // menu outside of it's parent container so the dropdown is not cut off by overflow hidden/scrolled parents.
   return <RSSelect
     className={cx("Select", "react-select", className)}
     classNamePrefix="react-select"
     isSearchable={isSearchable}
+    classNames={{
+      menu: (state) => state.placement === 'top' ? 'menu-above' : 'menu-below'
+    }}
+    menuPortalTarget={document.body}
+    menuPosition="fixed"
     {...otherProps}
   />
 }
